d3f7705724759702e00b66578e6e361e563db8b9
tzukas <tzukas@smail.uni-koeln.de>
2021-06-14 11:07:11 +0200

    Update cli.py
    Updates cli.py to transform data.

--- yellowcab/cli.py
+++ yellowcab/cli.py
@@ -1,22 +1,31 @@
 import click
+from yellowcab.cabana import trips_info
+from . import io
+from . import cabana
 from . import model
 
 
 @click.command()
-@click.option('--transform/--no-transform', default=False)
-@click.option('-i', default=None)
-@click.option('-o', default=None)
+@click.option('--transform/--no-transform', default=False, help="Transform dataset into the augmented format and "
+                                                                "save to disk")
+@click.option('-i', type=click.File('rb'), default=None, help="Input file")
+@click.option('-o', type=click.File('wb'), default=None, help="Output file")
 @click.option('--predict/--no-predict', default=False)
 def main(transform, i, o, predict):
     if transform:
+        cabana.trips_info(i).get_duration()
+        cabana.trips_info(i).get_time()
+        cabana.trips_info(i).get_position()
+
         if i is None or o is None:
             print("Insufficient file specification, use -i for input path and -o for output path")
         else:
             print("Transforming data ...")
+
     elif predict:
-        print("Predicting stuff...")
+        io.input.read_model()
+        print("Predicting...")
 
 
 if __name__ == '__main__':
     main()
-
